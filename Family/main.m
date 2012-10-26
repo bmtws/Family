fcc3866d76e5947d3cea0afefe838366439030a9
Benjamin Moreno Torres <Benyo@MacBook-Air-de-Benjamin.local>
2012-10-25 20:32:29 -0400

    Initial Commit

---
+++ Family/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Family
+//
+//  Created by Benjamin Moreno Torres on 25/10/12.
+//  Copyright (c) 2012 Benjamin Moreno Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
